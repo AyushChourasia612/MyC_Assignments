af9be4eeb0821806610ce5ad53ddc1bede06d76b
AyushChourasia612 <132686289+AyushChourasia612@users.noreply.github.com>
2023-05-25 21:55:57 +0530

    Add files via upload

---
+++ project-1.py.py
@@ -0,0 +1,16 @@
+#Author: Ayush Chourasia
+#Date of Creation: 18/5/22
+
+#Question 1
+print('Assignment 1: Write a program that accepts radius of a circle and prints its area.\n  ')
+
+rad = float(input('Please enter the radius of the circle : \n '))
+
+area = 3.14*rad**2
+
+print('Radius of Circle : ',rad)
+print('Area of Circle   : ',area)
+
+#Question 2
+f = input("Input the Filename:- \n ")
+print("extension of file is: ","'python'") 
